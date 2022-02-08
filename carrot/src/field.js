414263da031b9311e805bed04183ea268ff33ca7
BokyungJeon <jjewell@naver.com>
2022-02-08 23:05:39 +0900

    carrot game

---
+++ carrot/src/field.js
@@ -0,0 +1,62 @@
+'use strict';
+import * as sound from './sound.js';
+
+const CARROT_SIZE = 80;
+
+export const ItemType = Object.freeze({
+  carrot: 'carrot',
+  bug: 'bug',
+});
+
+export class Field {
+  constructor(carrotCount, bugCount) {
+    this.carrotCount = carrotCount;
+    this.bugCount = bugCount;
+    this.field = document.querySelector('.game__field');
+    this.fieldRect = this.field.getBoundingClientRect();
+    // this.onClick = this.onClick.bind(this) // this를 바인딩하는 방법 1.
+    this.field.addEventListener('click', this.onClick); // this를 바인딩하는 방법 2. (event) => this.onClick(event) arrow function으로 감싸면 this binding된다.
+  }
+
+  init() {
+    this.field.innerHTML = '';
+    this._addItem('carrot', this.carrotCount, 'img/carrot.png');
+    this._addItem('bug', this.bugCount, 'img/bug.png');
+  }
+
+  setClickListener(onItemClick) {
+    this.onItemClick = onItemClick;
+  }
+  _addItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = this.fieldRect.width - CARROT_SIZE;
+    const y2 = this.fieldRect.height - CARROT_SIZE;
+    for (let i = 0; i < count; i++) {
+      const item = document.createElement('img');
+      item.setAttribute('class', className);
+      item.setAttribute('src', imgPath);
+      item.style.position = 'absolute';
+      const x = randomNumber(x1, x2);
+      const y = randomNumber(y1, y2);
+      item.style.left = `${x}px`;
+      item.style.top = `${y}px`;
+      this.field.appendChild(item);
+    }
+  }
+  // this binding하는 방법 3. 함수를 변수로 만들고 arrow function을 가리킨다.
+  onClick = (event) => {
+    const target = event.target;
+    if (target.matches('.carrot')) {
+      target.remove();
+      sound.playCarrot();
+      this.onItemClick && this.onItemClick(ItemType.carrot);
+    } else if (target.matches('.bug')) {
+      this.onItemClick && this.onItemClick(ItemType.bug);
+    }
+  };
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
